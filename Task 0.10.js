eb46e4f1cafaa87828e56e3376c40c90e80b0d8f
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-05-31 22:59:23 -0700

    Update Task 0.10.js

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,15 +1,15 @@
-function commonLetters(str1,str2){     // logic for finding common letters
+function commonLetters(firstWord,secondWord){     // logic for finding common letters
     //wordOne = str1.split(",")
     var common = "";
-    str1 = str1.split('');
-    str2 = str2.split(''); 
-    str1.forEach(x =>{
-        if (str2.includes(x)){
-            common +=  str2[str2.indexOf(x)] ;
-            str2.splice(str2.indexOf(x), 1);
+    firstWord = firstWord.split('');
+    secondWord = secondWord.split(''); 
+    firstWord.forEach(x =>{
+        if (secondWord.includes(x)){
+            common +=  secondWord[secondWord.indexOf(x)] ;
+            secondWord.splice(secondWord.indexOf(x), 1);
         }
     })
     console.log(common);
-    return common;
+   
 }
     commonLetters('House','Computers');
