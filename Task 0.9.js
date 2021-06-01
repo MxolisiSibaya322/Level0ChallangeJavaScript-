0b591ccb2f24c961ee66dab01f146210c37d87ce
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-06-01 02:14:31 -0700

    Update Task 0.9.js

--- Task 0.9.js
+++ Task 0.9.js
@@ -3,7 +3,7 @@ function vowels( word){
     for(var i=0;i<= 'Word'.length ; i++){
         if ((word[i]=='a')||(word[i]=='A')||(word[i]=='e')||(word[i]=='E')||(word[i]=='i')||(word[i]=='I')||(word[i]=='o')||(word[i]=='O')||(word[i]=='u')||(word[i]=='U')){
             if (i!= 0){
-                vowels += " , " ;
+                vowels += ", " ;
             }
             vowels += word[i];      //finding vowels
         }
