18e87083544236bfb3d3869f53950dca33b84afb
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-06-01 02:35:15 -0700

    Update Task 0.8.js

--- Task 0.8.js
+++ Task 0.8.js
@@ -2,7 +2,7 @@ function convertToTime(num){
     var hours = Math.floor(num / 60) ;
     var minutes = num -(60* hours);
     if ((hours==1)&&(minutes==1)){
-        console.log(hours+ ' hour ' + minutes + ' minute');    //logic to deal with plurals
+        console.log(hours+ ' hour ' + minutes + ' minute');   
     }else if (minutes==1){ 
         console.log(hours+ ' hours ' + minutes + ' minute');
     }else if(hours==1){
