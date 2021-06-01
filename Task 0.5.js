ff0489c3c6d304846070a08d6e3943f222fc54f0
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-06-01 02:34:03 -0700

    Update Task 0.5.js

--- Task 0.5.js
+++ Task 0.5.js
@@ -1,4 +1,4 @@
-function areaOfTriangle(side1,side2,side3){        //calculating area of a triangle
+function areaOfTriangle(side1,side2,side3){       
     var s= 0.5*(side1+ side2 + side3);
     var areaTotal =  Math.sqrt(s*(s-side1)*(s-side2)*(s-side3))
     return areaTotal;
