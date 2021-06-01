ea77e2af4a585595601fcde201065c800bf2e571
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-05-31 22:36:46 -0700

    Update Task 0.7.js
    returning only temperature

--- Task 0.7.js
+++ Task 0.7.js
@@ -1,11 +1,11 @@
 function celsiusToFahrenheit(temp){
     var fahrenheit = (temp * 9/5) + 32 ;    // converting to Fahrenheit
     
-    return  temp + ' Degrees celsius is equal to ' + fahrenheit + ' Fahrenheit' ;
+    return   fahrenheit  ;
 } function fahrenheitToCelcius(temp){
     var celcius = (temp-32)* (5/9); 
     
-    return  temp + ' Fahrenheit = '+ celcius + ' celcius';
+    return  celcius ;
 }
 celsiusToFahrenheit(32);
 fahrenheitToCelcius(89.6);
