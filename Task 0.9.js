6b8d449f20159faa972486338b3be4938ad8dcea
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-05-31 22:45:39 -0700

    Update Task 0.9.js

--- Task 0.9.js
+++ Task 0.9.js
@@ -1,9 +1,13 @@
 function vowels( word){
-    
+    let vowels = "";
     for(var i=0;i<= 'Word'.length ; i++){
         if ((word[i]=='a')||(word[i]=='A')||(word[i]=='e')||(word[i]=='E')||(word[i]=='i')||(word[i]=='I')||(word[i]=='o')||(word[i]=='O')||(word[i]=='u')||(word[i]=='U')){
-            console.log('Vowel : '+ word[i]);      //finding vowels
+            if (i!= 0){
+                vowels += " , " ;
+            }
+            vowels += word[i];      //finding vowels
         }
     }
+    console.log(vowels);
 }
 vowels('Umuzi');     
