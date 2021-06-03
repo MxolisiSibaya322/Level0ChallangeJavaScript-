e0b3994b51a4bcd81a3a3dcc9277b526cf33559e
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-06-02 23:19:05 -0700

    Update Task 0.8.js

--- Task 0.8.js
+++ Task 0.8.js
@@ -2,13 +2,13 @@ function convertToTime(num){
     var hours = Math.floor(num / 60) ;
     var minutes = num -(60* hours);
     if ((hours==1)&&(minutes==1)){
-        console.log(hours+ ' hour ' + minutes + ' minute');   
+        console.log(hours+ ' hour, ' + minutes + ' minute');   
     }else if (minutes==1){ 
-        console.log(hours+ ' hours ' + minutes + ' minute');
+        console.log(hours+ ' hours, ' + minutes + ' minute');
     }else if(hours==1){
-        console.log(hours+ ' hour ' + minutes + ' minutes');
+        console.log(hours+ ' hour, ' + minutes + ' minutes');
     }else{
-        console.log(hours+ ' hours ' + minutes + ' minutes');
+        console.log(hours+ ' hours, ' + minutes + ' minutes');
     }
 }
 convertToTime(183);  
