3010f8e286bcb11810080af944779218d47c98ed
Your Name <you@example.com>
2021-10-14 17:33:13 -0700

    Task 0 formatted

--- Task 0.7.js
+++ Task 0.7.js
@@ -1,11 +1,9 @@
 function celsiusToFahrenheit(temp) {
   const fahrenheit = (temp * 9) / 5 + 32;
-
   return fahrenheit;
 }
 function fahrenheitToCelcius(temp) {
   const celcius = (temp - 32) * (5 / 9);
-
   return celcius;
 }
 celsiusToFahrenheit(32);
